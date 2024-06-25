36d4586e200d3901ea3b4a710427a6c243fd2642
Nouman Sharif <noumansharifgul27@gmail.com>
2024-06-25 17:27:10 +0500

    conpleted practice

---
+++ README.md
@@ -0,0 +1,3 @@
+#GSAP scrollTrigger
+
+understanding gsap scrollTrigger events in a good way.
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 gsap.to("#box-1 .child-box", {
-  delay: 1,
+  delay: 0.5,
   scale: 1.5,
   rotate: "30deg",
-  duration: 2,
+  duration: 1.5,
   ease: Power4.easeInOut,
   // scrollTrigger: {
   //   trigger: ".child-box",
@@ -10,15 +10,34 @@ gsap.to("#box-1 .child-box", {
   //   marker: true,
   // },
 });
+
 gsap.to("#box-2 .child-box", {
-  delay: 1,
+  delay: 0.5,
   scale: 1.5,
   rotate: "-30deg",
-  duration: 2,
+  duration: 1.5,
+  opacity: 0,
   ease: Power4.easeInOut,
   scrollTrigger: {
     trigger: "#box-2 .child-box",
     scroller: "body",
     markers: true,
+    start: "top 60%",
+    end: "top 30%",
+    scrub: 1,
+    pin: true,
+  },
+});
+
+gsap.to("#box-3 .child-box", {
+  delay: 0.5,
+  scale: 0.5,
+  duration: 1.5,
+  ease: Expo.easeInOut,
+  scrollTrigger: {
+    trigger: "#box-3 .child-box",
+    scroller: "body",
+    markers: true,
+    scrub: true,
   },
 });
